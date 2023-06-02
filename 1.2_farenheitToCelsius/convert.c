2df0ff5b30d69665e0abb28c92527f17f62380b7
Manuel Espinosa <106558272+manuelesvi@users.noreply.github.com>
2023-06-02 14:30:55 -0600

    Update convert.c
    Use floating points instead of integrals for Fº to ºC conversion.

--- 1.2_farenheitToCelsius/convert.c
+++ 1.2_farenheitToCelsius/convert.c
@@ -16,7 +16,7 @@ int main()
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
+        celsius = (5.0/9.0) * (fahr-32.0);
 	printf("%3.0f %6.1f\n", fahr, celsius);
 	fahr = fahr + step;
     }
