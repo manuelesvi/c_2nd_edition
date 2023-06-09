8d67065fea79c4844f33e102daef034de1a13ea7
Manuel Espinosa <manuel.espinosa@live.com>
2023-06-09 09:12:07 -0600

    1.9 Call by Value

---
+++ chapter1_a_tutorial_introduction/1.8_callByValue/1.9_callByValue.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int power(int, int);
+
+/* test power function */
+int main()
+{
+    int i;
+
+    for(i = 0; i < 10; ++i)
+        printf("%d %d %d\n", 1, power(2,i), power(-3,i));
+    return 0;
+}
+
+/* power: raise a base to n-th power; n >= 0; version 2 */
+int power(int base, int n)
+{
+    int p;
+
+    for (p = 1; n > 0; --n) // changing value (n) doesn't have any effect on calling argument (i)
+        p = p * base;
+    return p;
+}
+
